c4049ae407d69813181ee460a0e7a5b75f10c5bf
carmartdev <amir8553mohammad@gmail.com>
2025-03-13 23:25:25 +0330

    Made it better (but broke it some how)
    I'm sleepy right now I'm going to bed tomorow I'll go trough the book
    and see what have I done wrong, Good night my future self

--- src/bin/2guess.rs
+++ src/bin/2guess.rs
@@ -1,9 +1,24 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
 fn main(){
+    let num = rand::thread_rng().gen_range(1..=101);
     println!("Guess the number!");
     println!("What's your number?");
     let mut guess = String::new();
-    io::stdin().read_line(&mut guess)
-	.expect("Failed to read line");
-    println!("You guessed: {}", guess);
+    loop{
+	io::stdin().read_line(&mut guess)
+	    .expect("Failed to read line");
+	let guess:u32 = guess.trim().parse()
+	    .expect("Type a proper number!");
+	match guess.cmp(&num){
+	    Ordering::Less => println!("Too Low"),
+	    Ordering::Greater => println!("Too High"),
+	    Ordering::Equal => {
+		println!("Correct, You Won!!!");
+		break;
+	    }
+	}
+    }
 }
